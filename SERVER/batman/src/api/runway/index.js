a2b7360fd631c891a3431e0de0250195aea430fb
jaemin <anjm1020@gmail.com>
2021-10-15 15:47:04 +0000

    update : CRUD test

--- SERVER/batman/src/api/runway/index.js
+++ SERVER/batman/src/api/runway/index.js
@@ -1,9 +1,11 @@
 import Router from "koa-router";
 
-import * as ruwnayCtrl from "./runway.ctrl";
+import * as runwayCtrl from "./runway.ctrl";
 
 const runway = new Router();
 
-runway.post("/load", ruwnayCtrl.save);
-
+runway.post("/", runwayCtrl.save);
+runway.get("/:_id",runwayCtrl.read);
+runway.patch("/:_id",runwayCtrl.update);
+runway.delete("/:_id",runwayCtrl.remove);
 export default runway;
--- SERVER/batman/src/api/runway/runway.ctrl.js
+++ SERVER/batman/src/api/runway/runway.ctrl.js
@@ -1,6 +1,56 @@
 import Runway from "../../model/Runway";
 
-export const save = (ctx) => {
-	const {data} = ctx.request.body;
-	ctx.body = data;
+export const save = async ( ctx ) => {
+	const {name,angles,top,bottom} = ctx.request.body;
+	const new_runway = new Runway({
+		name,
+		angles,
+		sectors:{
+			top:top,
+			bottom:bottom
+		}
+	});
+	
+	try{
+		const res = await new_runway.save();
+		ctx.body=res;
+	} catch(e){
+		ctx.status=400;
+		ctx.body=e;
+	}
 }
+
+export const read = async ( ctx ) => {
+	const {_id} = ctx.params;
+	try{
+		const runway = await Runway.findOne({_id}).exec();
+		if(!runway)
+			ctx.body = "No Runway";
+		ctx.body = runway;
+	} catch (e){
+		ctx.body = e;
+	}
+}
+
+export const update = async ( ctx ) => {
+	const {_id} = ctx.params;
+	const body = ctx.request.body;
+	try{
+		const res = await Runway.updateOne({_id},body);
+		ctx.status = 204;
+	} catch(e){
+		ctx.status=400;
+	}
+}
+
+export const remove = async ( ctx ) => {
+	const {_id} = ctx.params;
+	try{
+		await Runway.deleteOne({_id});
+		ctx.status=204;
+	} catch(e){
+		ctx.status=400;
+	}
+}
+
+// basic CRUD
